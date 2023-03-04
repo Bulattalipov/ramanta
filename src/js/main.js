3ea16ed42ec5d687a32d5cf0a143b651cc6ae780
Bulat <bulat0talipov@gmail.com>
2023-03-04 23:34:39 +0300

    update 04.03.2023

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import lazyIMages from './modules/lazyIMages';
 import documenReady from './helpers/documenReady';
 import initModal from './modules/initModal';
 import onMenuToggle from './modules/onMenuToggle';
+import phoneMask from './helpers/phoneMask';
 
 documenReady(() => {
   window.___YOUR_PROJECT___API = {};
@@ -11,4 +12,5 @@ documenReady(() => {
   lazyIMages();
   initModal();
   onMenuToggle();
+  phoneMask();
 });
